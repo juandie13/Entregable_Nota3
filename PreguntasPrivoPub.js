10f3b90a935b2bf213db7ffac585dc6aada692c2
Juan Diego Castillo Carnero <juandiegomaster13@hotmail.com>
2022-05-16 23:05:08 -0500

    Estas son las partes de JD

---
+++ PreguntasPrivoPub.js
@@ -0,0 +1,36 @@
+function validar1()
+{
+    console.log("Se envió el nombre");
+    formulario1.reset();
+    return false;
+}
+function validar2()
+{
+    console.log("Se envió el nombre");
+    formulario2.reset();
+    return false;
+}
+function validar3()
+{
+    console.log("Se envió el nombre");
+    formulario3.reset();
+    return false;
+}
+function validar4()
+{
+    console.log("Se envió el nombre");
+    formulario4.reset();
+    return false;
+}
+function validar5()
+{
+    console.log("Se envió el nombre");
+    formulario5.reset();
+    return false;
+}
+function validar6()
+{
+    console.log("Se envió el nombre");
+    formulario6.reset();
+    return false;
+}
